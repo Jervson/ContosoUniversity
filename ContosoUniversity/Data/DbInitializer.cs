7e5e2da81704fd4af524c56ed6c4f7045fe5c8bc
Jervson <henri.jervson@gmail.com>
2023-09-01 15:46:58 +0300

    database info

---
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -0,0 +1,69 @@
+﻿using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Students.Any())
+            {
+                return;
+            }
+
+            var students = new Student[]
+            {
+                new Student() {FirstMidName="Suss", LastName="Mägi", EnrollmentDate=DateTime.Parse("1973-09-01") },
+                new Student() {FirstMidName="Juss", LastName="Mägi", EnrollmentDate=DateTime.Parse("1973-09-01") },
+                new Student() {FirstMidName="Vuss", LastName="Mägi", EnrollmentDate=DateTime.Parse("1973-09-01") },
+                new Student() {FirstMidName="Luss", LastName="Mägi", EnrollmentDate=DateTime.Parse("1973-09-01") },
+                new Student() {FirstMidName="Kuss", LastName="Mägi", EnrollmentDate=DateTime.Parse("1973-09-01") }
+            };
+
+            foreach (Student s in students) 
+            {
+                context.Students.Add(s);    
+            }
+            context.SaveChanges();
+
+            var courses = new Course[]
+            {
+                new Course() {CourseID=1050, Title="Programming", Credits=160},
+                new Course() {CourseID=2050, Title="Chemistry", Credits=160},
+                new Course() {CourseID=3050, Title="Mathematics", Credits=160},
+                new Course() {CourseID=4050, Title="PE", Credits=160},
+                new Course() {CourseID=5050, Title="Physics", Credits=160}
+            };
+
+            foreach (Course c in courses)
+            {
+                context.Courses.Add(c);
+            }
+            context.SaveChanges();
+
+            var enrollments = new Enrollment[]
+            {
+                new Enrollment() {StudentID=1, CourseID=1050, Grade=Grade.A},
+                new Enrollment() {StudentID=3, CourseID=2050, Grade=Grade.B},
+                new Enrollment() {StudentID=3, CourseID=3050, Grade=Grade.C},
+                new Enrollment() {StudentID=4, CourseID=4050, Grade=Grade.D},
+                new Enrollment() {StudentID=5, CourseID=5050, Grade=Grade.F},
+                new Enrollment() {StudentID=5, CourseID=1050, Grade=Grade.A},
+                new Enrollment() {StudentID=4, CourseID=3050, Grade=Grade.B},
+                new Enrollment() {StudentID=3, CourseID=5050, Grade=Grade.C},
+                new Enrollment() {StudentID=3, CourseID=4050, Grade=Grade.D},
+                new Enrollment() {StudentID=1, CourseID=5050, Grade=Grade.F},
+                new Enrollment() {StudentID=1, CourseID=2050, Grade=Grade.A},
+                new Enrollment() {StudentID=4, CourseID=2050, Grade=Grade.B}
+            };
+
+            foreach (Enrollment c in enrollments)
+            {
+                context.Enrollments.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,27 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        CreateDbIfNotExists(host);
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultController")));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+    private static void CreateDbIfNotExists(IHost host)
+    {
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<SchoolContext>();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured creating the database");
+            }
+
+        }
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Program>();
+            });
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
